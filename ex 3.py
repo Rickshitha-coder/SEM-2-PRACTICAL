26e81aa7c4dd91ea7c710890efa5a11c72b638f5
Rickshitha-coder <rickshithaanandakumar@gmail.com>
2024-11-28 18:31:56 +0530

    Add files via upload

---
+++ ex 3.py
@@ -0,0 +1,19 @@
+print("The Area of Square")
+side=int(input("Enter the Side Of Square:"))
+area=side*side
+print(f"The Area of Square is :{area}")
+print("The Area of Rectangle")
+length=int(input("Enter the Length:"))
+breadth=int(input("Enter the Breadth:"))
+area_rec=length *breadth
+print(f"The Are of Rectangle is:{area_rec}")
+print("The Area of Triangle")
+heigth=int(input("Enter the Hiegth:"))
+breath_tri=int(input("Enter the breadth of Triangle:"))
+area_tri=(1/2*(breath_tri*heigth))
+print(f"The Area of triangle:{area_tri}")
+print("The Area of circle")
+radius=int(input("Enter the Radius :"))
+area_cir=(3.14)*(radius*radius)
+print(f"The Area of Circle is:{area_cir}")
+
