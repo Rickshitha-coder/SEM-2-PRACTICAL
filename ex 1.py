5932f49f159c38c3b9f3621ac5d3501620541778
Rickshitha-coder <rickshithaanandakumar@gmail.com>
2024-12-12 09:14:57 +0530

    Update ex 1.py

--- ex 1.py
+++ ex 1.py
@@ -5,4 +5,10 @@ if choice=='c':
     celsius=float(input("Enter temperature in Celsius:"))
     fahrenheit=(celsius*9/5)+32
     print('%.2f Celsius is:%0.2f Fahrenheit'%(celsius,fahreheit))
-elif choice==
+elif choice=='f':
+    fahrenheit=float(input("Enter temperature in fahrenheit:"))
+    celsius=(fahrenheit-32)*5/9
+    print('%.2f Fahrenheit is:%0.2f Celsius'%(fahrenheit,celsius))
+else:
+    print("Invaild Input")
+
